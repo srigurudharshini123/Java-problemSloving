d9245dd0991584eeddc2ac40f2e505192c20bd43
srigurudharshini123 <srigurudharshini@gmail.com>
2026-06-02 18:44:32 +0530

    Create problem-2.java

---
+++ day-7/problem-2.java
@@ -0,0 +1,41 @@
+// print the number of subarray having negative sums:
+// input: length:5
+// array elements: 1 -2 4 -5 1
+// output: 9
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
